822704d526b38752be4b5ec58646a731c0fd2d2e
Yannick Simon <yannick.simon@gmail.com>
2023-02-09 21:54:00 +0100

    Removed history

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from config import config
+
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    from flask_cors import CORS
+
+    app = Flask(__name__)
+    CORS(app)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    db.init_app(app)
+    return app
